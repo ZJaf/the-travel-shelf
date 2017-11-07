a364b3cb75c36516346afc7bdc91ed263a12221e
Hanie <haniejafarii@gmail.com>
2017-11-07 17:41:40 -0500

    Fix issue with router by moving router setup to the app.js instead of nav_bar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Homepage from './scenes/Homepage';
+import BookstoreList from './scenes/BookstoreList';
 import NavBar from '././components/nav_bar.js';
 
 class App extends Component {
+
   render() {
     return (
+        <Router>
           <div>
             <NavBar />
+            <Route exact path="/" component={Homepage}/>
+            <Route path="/BookStoreList" component={BookstoreList}/>            
           </div>
+        </Router>
     );
   }
 }
--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -4,48 +4,43 @@ import {
     Route,
     Link
 } from 'react-router-dom';
-import Homepage from '../scenes/Homepage/index';
-import BookstoreList from '../scenes/BookstoreList/index';
 
 class Navbar extends Component {
+
   render(){
     return (
-           <Router>
-              <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                <div className="container">
-                  <a className="navbar-brand" href="#">
-                    <img src={require('../img/ts-logo.jpg')} width="50" height="40" alt="travel-shelf logo" /><h4 className="d-inline align-middle"> Travel Shelf</h4>
-                  </a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                      <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                      </li>
-                      <li className="nav-item">
-                        <a href="#about" className="nav-link">About</a>
-                      </li>
-                      <li className="nav-item">
-                        <Link to="/BookStoreList" className="nav-link">BookStores</Link>
-                      </li>
-                      <li className="nav-item">
-                        <a href="#contact" className="nav-link">Contact</a>
-                      </li>
-                      <li className="nav-item">
-                        <a href="#sign-up" className="nav-link">Sign Up</a>
-                      </li>
-                      <li className="nav-item">
-                        <a href="#login" className="nav-link">Login</a>
-                      </li>
-                    </ul>
-                    <Route exact path="/" component={Homepage}/>
-                    <Route path="/BookStoreList" component={BookstoreList}/>
-                  </div>
-                </div>
-              </nav>
-          </Router>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={require('../img/ts-logo.jpg')} width="50" height="40" alt="travel-shelf logo" /><h4 className="d-inline align-middle"> Travel Shelf</h4>
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <Link to="/BookStoreList" className="nav-link">BookStores</Link>
+              </li>
+              <li className="nav-item">
+                <a href="#contact" className="nav-link">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a href="#sign-up" className="nav-link">Sign Up</a>
+              </li>
+              <li className="nav-item">
+                <a href="#login" className="nav-link">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
         )
     }
   };
